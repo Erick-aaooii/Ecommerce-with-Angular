ed3c8a528bea4a5d7f56827153e0bacce8a6a1da
Erick-aaooii <andreserick2004@gmail.com>
2025-05-21 22:58:11 -0300

    feat: search bar for prouct route

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from './components/product/product.component';
+import { SearchProductsComponent } from "./components/searchProducts/searchProducts.component";
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [ ProductComponent ],
+  imports: [ProductComponent, SearchProductsComponent],
   templateUrl: './produtos.component.html'
 })
 export class ProdutosComponent {
